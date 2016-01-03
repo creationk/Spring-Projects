49ca8a2d6c42bc61df599fc40ac20489b656ff51
creationk <creationk@users.noreply.github.com>
2016-01-03 21:33:43 +0530

    Creating Spring Project with jars included

---
+++ SpringHelloWorld/src/main/java/com/spring/helloworld/controller/DefaultController.java
@@ -0,0 +1,15 @@
+package com.spring.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("private_link", "http://localhost:8080/SpringHelloWorld/srujana.private");
+		model.addAttribute("public_link", "http://localhost:8080/SpringHelloWorld/srujana.public");
+		return "index";
+	}
+}
---
+++ SpringHelloWorld/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,16 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<!-- <link rel="stylesheet"
+	href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"> -->
+<title>Welcome</title>
+</head>
+<body>
+	<section>
+		<h4><a href="${private_link}">Private Page</a></h4>
+		<h4><a href="${public_link}">Public Page</a></h4>
+	</section>
+</body>
+</html>
+
